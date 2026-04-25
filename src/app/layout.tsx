1feef37278f80a0d523816d039df174a2c98f3f9
hro555 <xxx70@mail.ru>
2026-04-25 20:05:50 +0200

    Remplacer gtag.js par Google Tag Manager GTM-K3K7SPBG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,25 +52,28 @@ export default function RootLayout({
   return (
     <html lang="fr" className={`${geistSans.variable} h-full antialiased`}>
       <head>
+        <Script id="gtm" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3K7SPBG');`}
+        </Script>
         <Script
           id="cookieyes"
           src="https://cdn-cookieyes.com/client_data/851647ee0a87e14f540d69b5/script.js"
           strategy="beforeInteractive"
         />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-18074344411"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-18074344411');
-          `}
-        </Script>
       </head>
       <body className="min-h-full flex flex-col bg-background text-foreground">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K3K7SPBG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <JsonLd />
         <Header />
         <main className="flex-1">{children}</main>
